32bf0e96892d88a93992536548d30d72394b07bf
ZoieBoyd <zoieboyd@outlook.com>
2026-05-27 16:45:23 +0100

    fix: Fix tests to work with altered code

--- src/modules/tests/gameboard.test.js
+++ src/modules/tests/gameboard.test.js
@@ -23,14 +23,14 @@ describe("Testing ship placement functionality", () => {
 
     test("Places ship of length 2 at an out of bounds location [10, 10]", () => {
         const board = new Gameboard();
-        expect(() => board.placeShip(new Ship(2, "patrol boat"), 10, 10, "horizontal")).toThrow();
+        expect(board.placeShip(new Ship(2, "patrol boat"), 10, 10, "horizontal")).toBeFalsy();
     });
 
     test("Place ship of length 5 at [1, 1] and another ship of length 3 in an interlacting location [1, 3]", () => {
         const board = new Gameboard();
         const carrier = new Ship(5, "carrier");
         board.placeShip(carrier, 1, 1, "horizontal");
-        expect(() => board.placeShip(new Ship(3, "destoyer"), 1, 3, "horizontal")).toThrow();
+        expect(board.placeShip(new Ship(3, "destroyer"), 1, 3, "horizontal")).toBeFalsy();
     });
 
     test("Places fleet comprised of randomly generated coordinates on gameboard", () => {
